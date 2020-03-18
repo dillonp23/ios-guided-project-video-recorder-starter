012ce429081b2e0ee95f402ce31821bc8dc73621
dillonp23 <dillonp.lambda@gmail.com>
2020-03-17 17:19:16 -0700

    Added calls to start and stop the session.

--- VideoRecorder/VideoRecorder/CameraViewController.swift
+++ VideoRecorder/VideoRecorder/CameraViewController.swift
@@ -59,6 +59,18 @@ class CameraViewController: UIViewController {
         }
         preconditionFailure("No cameras on device match the specs that we need")
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        captureSession.startRunning()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        captureSession.stopRunning()
+    }
 
     @IBAction func recordButtonPressed(_ sender: Any) {
 
